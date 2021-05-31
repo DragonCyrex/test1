3f35caed9417030a4c92b90978bd86ac8587d950
TawfiqMohammd <>
2021-05-31 16:41:41 +0000

    added dynamic java stuff

---
+++ index.js
@@ -0,0 +1,7 @@
+userName = prompt("What is your name?");
+if (userName == "Tawfeeq")
+{alert("we Love you Tawfeeq");}
+else {alert("Intruder Alert!");}
+document.write("Hello There! " + userName + " How can i help you?");
+alert("you are awesome " + userName);
+confirm("just wanted to let you know!");
