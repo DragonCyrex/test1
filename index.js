afc6ed565ab8e4641eef95900b9a3e0d8e9e529e
TawfiqMohammd <>
2021-05-31 22:59:51 +0000

    added CSS

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-userName = prompt("What is your name?");
-
-if (userName == "Tawfeeq"){
+var userName = prompt("What is your name?");
+function name (x){
+if (x == "Tawfeeq"){
   alert("we Love you Tawfeeq");
   }
-else if (userName == "Waad"){
+else if (x == "Waad"){
   alert("Waad don't enter the website");
   }
 else {
   alert("Intruder Alert!");
 }
+document.write("Hello There! " + x + " How can i help you?");
+alert("you are awesome " + x);
+}
+name(userName);
 
-document.write("Hello There! " + userName + " How can i help you?");
-alert("you are awesome " + userName);
 
+function age (){
 var x = confirm("just wanted to let you know!");
 if (x == true){
   alert("Thank you");
@@ -31,3 +34,5 @@ var graphic_Design = prompt("how much do you rate Graphic Design? -out of 10")
 for (i = 0; i < graphic_Design; i++){
   document.write("<img src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Star_empty.svg/1005px-Star_empty.svg.png'>")
 }
+}
+age()
