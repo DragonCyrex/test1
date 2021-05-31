f0fce746b8d2f93dd452ded2bcf17e3430f0b683
TawfiqMohammd <>
2021-05-31 16:56:34 +0000

    added if statement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 userName = prompt("What is your name?");
 if (userName == "Tawfeeq")
 {alert("we Love you Tawfeeq");}
+else if (userName == "Waad"){alert("Waad don't enter the website");}
 else {alert("Intruder Alert!");}
 document.write("Hello There! " + userName + " How can i help you?");
 alert("you are awesome " + userName);
